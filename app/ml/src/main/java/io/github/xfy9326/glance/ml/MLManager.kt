05d076234d66aa1a0cc54070a74a6ba9b6df091b
xfy9326 <xfy9326@qq.com>
2022-06-07 23:59:43 +0100

    Replace general model fp32 with fp16 version

--- app/ml/src/main/java/io/github/xfy9326/glance/ml/MLManager.kt
+++ app/ml/src/main/java/io/github/xfy9326/glance/ml/MLManager.kt
@@ -11,8 +11,11 @@ object MLManager {
     private const val DEFAULT_CONF_THRESHOLD = 0.25f
     private const val DEFAULT_IOU_THRESHOLD = 0.45f
 
+    // yolov5n_fp16_opt
     private const val MODEL_GUIDE_BIN_PATH = "models/guide.bin"
     private const val MODEL_GUIDE_PARAM_BIN_PATH = "models/guide.param.bin"
+
+    // yolov5s_fp16_opt
     private const val MODEL_GENERAL_BIN_PATH = "models/general.bin"
     private const val MODEL_GENERAL_PARAM_BIN_PATH = "models/general.param.bin"
 
