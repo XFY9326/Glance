a0dc52d219b3da5f31baa47e91a1afaf0a1fffb8
xfy9326 <xfy9326@qq.com>
2022-06-08 15:34:31 +0100

    Enable split abi apk for release build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,6 +31,15 @@ android {
             packagingOptions {
                 resources.excludes += "DebugProbesKt.bin"
             }
+
+            splits {
+                abi {
+                    isEnable = true
+                    reset()
+                    include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+                    isUniversalApk = true
+                }
+            }
         }
     }
 
