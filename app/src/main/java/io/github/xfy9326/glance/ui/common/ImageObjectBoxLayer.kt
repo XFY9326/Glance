9a586aa062c04f3132a6803d89fc84bfca35bea4
xfy9326 <xfy9326@qq.com>
2022-07-10 00:16:10 +0100

    Reimplement accessibility layer with compose

--- app/src/main/java/io/github/xfy9326/glance/ui/common/ImageObjectBoxLayer.kt
+++ app/src/main/java/io/github/xfy9326/glance/ui/common/ImageObjectBoxLayer.kt
@@ -1,33 +1,27 @@
 package io.github.xfy9326.glance.ui.common
 
-import android.content.Context
-import android.graphics.Rect
-import android.os.Bundle
-import android.view.KeyEvent
-import android.view.MotionEvent
-import android.view.View
-import android.view.ViewGroup
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.border
+import androidx.compose.foundation.focusable
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.layout.SubcomposeLayout
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.viewinterop.AndroidView
-import androidx.core.graphics.toRect
-import androidx.core.view.ViewCompat
-import androidx.core.view.accessibility.AccessibilityNodeInfoCompat
-import androidx.customview.widget.ExploreByTouchHelper
-import io.github.xfy9326.glance.ui.data.*
+import io.github.xfy9326.glance.ui.data.ImageObject
+import io.github.xfy9326.glance.ui.data.ImageObjectInfo
+import io.github.xfy9326.glance.ui.data.fitLayoutSize
 import io.github.xfy9326.glance.ui.theme.AppTheme
 
 
@@ -62,115 +56,50 @@ fun ImageObjectBoxLayer(
     boxStroke: Dp = 2.dp,
     boxStrokeColor: Color = Color.Red,
     cornerRadius: Dp = 2.dp,
-    accessibilityInteractive: Boolean = true
 ) {
     val targetBoxStroke = if (boxStroke == Dp.Hairline) {
         (1f / LocalDensity.current.density).dp
     } else {
         boxStroke
     }
-    Box(modifier = modifier) {
-        Canvas(
-            modifier = Modifier.fillMaxSize(),
-            onDraw = {
-                val stroke = Stroke(width = targetBoxStroke.toPx())
-                val radius = CornerRadius(cornerRadius.toPx(), cornerRadius.toPx())
-                imageObjectInfo.calculateLayout(size.width, size.height).forEach {
-                    drawRoundRect(
-                        color = boxStrokeColor,
-                        topLeft = it.boxOffset,
-                        size = it.boxSize,
-                        style = stroke,
-                        cornerRadius = radius
+    SubcomposeLayout(
+        modifier = modifier,
+        measurePolicy = { constraints ->
+            val layoutInfo = imageObjectInfo.fitLayoutSize(constraints.maxWidth, constraints.maxHeight)
+            val placeables = subcompose(Unit) {
+                layoutInfo.map {
+                    ImageObjectBox(
+                        label = it.imageObject.classText,
+                        boxStroke = targetBoxStroke,
+                        boxStrokeColor = boxStrokeColor,
+                        cornerRadius = cornerRadius
                     )
                 }
+            }.mapIndexed { i, measurable ->
+                val size = layoutInfo[i].boxSize
+                measurable.measure(Constraints.fixed(size.width, size.height))
             }
-        )
-        if (accessibilityInteractive) {
-            AndroidView(
-                modifier = Modifier.fillMaxSize(),
-                factory = { context ->
-                    ImageObjectBoxAccessibilityLayer(context).also {
-                        it.layoutParams = ViewGroup.LayoutParams(
-                            ViewGroup.LayoutParams.MATCH_PARENT,
-                            ViewGroup.LayoutParams.MATCH_PARENT
-                        )
-                        it.setImageObjects(imageObjectInfo)
-                    }
+            layout(constraints.maxWidth, constraints.maxHeight) {
+                placeables.forEachIndexed { i, placeable ->
+                    placeable.placeRelative(layoutInfo[i].boxOffset)
                 }
-            )
+            }
         }
-    }
+    )
 }
 
-private class ImageObjectBoxAccessibilityLayer(context: Context) : View(context) {
-    private val touchHelper = ImageObjectBoxTouchHelper()
-    private var imageObjectInfo: ImageObjectInfo? = null
-    private val imageObjectLayoutInfoList = ArrayList<ImageObjectLayoutInfo>(0)
-
-    init {
-        ViewCompat.setAccessibilityDelegate(this, touchHelper)
-    }
-
-    fun setImageObjects(imageObjectInfo: ImageObjectInfo) {
-        this.imageObjectInfo = imageObjectInfo
-    }
-
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        imageObjectInfo?.calculateLayout(measuredWidth.toFloat(), measuredHeight.toFloat())?.also {
-            imageObjectLayoutInfoList.clear()
-            imageObjectLayoutInfoList.addAll(it)
-        }
-    }
-
-    override fun dispatchHoverEvent(event: MotionEvent): Boolean {
-        return (touchHelper.dispatchHoverEvent(event) || super.dispatchHoverEvent(event))
-    }
-
-    override fun dispatchKeyEvent(event: KeyEvent): Boolean {
-        return (touchHelper.dispatchKeyEvent(event) || super.dispatchKeyEvent(event))
-    }
-
-    override fun onFocusChanged(gainFocus: Boolean, direction: Int, previouslyFocusedRect: Rect?) {
-        super.onFocusChanged(gainFocus, direction, previouslyFocusedRect)
-        touchHelper.onFocusChanged(gainFocus, direction, previouslyFocusedRect)
-    }
-
-    private inner class ImageObjectBoxTouchHelper : ExploreByTouchHelper(this) {
-
-        override fun getVirtualViewAt(x: Float, y: Float): Int {
-            for ((i, it) in imageObjectLayoutInfoList.withIndex()) {
-                if (it.getAndroidRectF().contains(x, y)) {
-                    return i
-                }
-            }
-            return HOST_ID
-        }
-
-        override fun getVisibleVirtualViews(virtualViewIds: MutableList<Int>) {
-            virtualViewIds.addAll(imageObjectLayoutInfoList.indices)
-        }
-
-        override fun onPopulateNodeForVirtualView(virtualViewId: Int, node: AccessibilityNodeInfoCompat) {
-            if (virtualViewId in imageObjectLayoutInfoList.indices) {
-                val data = imageObjectLayoutInfoList[virtualViewId]
-                val screenOffset = IntArray(2).also { getLocationOnScreen(it) }
-                val parentBounds = data.getAndroidRectF().toRect()
-                @Suppress("DEPRECATION")
-                node.setBoundsInParent(parentBounds)
-                val screenBounds = parentBounds.also {
-                    it.offset(screenOffset[0], screenOffset[1])
-                }
-                node.setBoundsInScreen(screenBounds)
-                node.contentDescription = data.imageObject.classText
-            } else {
-                error("Invalid virtual view id $virtualViewId!")
-            }
-        }
-
-        override fun onPerformActionForVirtualView(virtualViewId: Int, action: Int, arguments: Bundle?): Boolean {
-            return false
-        }
-    }
+@Composable
+private fun ImageObjectBox(
+    label: String,
+    boxStroke: Dp,
+    boxStrokeColor: Color,
+    cornerRadius: Dp
+) {
+    Spacer(
+        modifier = Modifier
+            .fillMaxSize()
+            .focusable()
+            .border(boxStroke, boxStrokeColor, RoundedCornerShape(cornerRadius))
+            .semantics { contentDescription = label }
+    )
 }
--- app/src/main/java/io/github/xfy9326/glance/ui/data/ImageObjectLayoutInfo.kt
+++ app/src/main/java/io/github/xfy9326/glance/ui/data/ImageObjectLayoutInfo.kt
@@ -1,42 +1,32 @@
 package io.github.xfy9326.glance.ui.data
 
-import android.graphics.RectF
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Rect
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.toAndroidRectF
-import kotlin.math.abs
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntSize
 import kotlin.math.min
+import kotlin.math.roundToInt
 
 data class ImageObjectLayoutInfo(
     val imageObject: ImageObject,
-    val boxOffset: Offset,
-    val boxSize: Size
+    val boxOffset: IntOffset,
+    val boxSize: IntSize
 )
 
-fun ImageObjectLayoutInfo.getAndroidRectF(): RectF =
-    Rect(boxOffset, boxSize).toAndroidRectF()
+fun ImageObjectInfo.fitLayoutSize(layoutWidth: Int, layoutHeight: Int): List<ImageObjectLayoutInfo> {
+    val ratio = min(layoutWidth / size.width, layoutHeight / size.height)
+    val scaledWidth = min(size.width * ratio, layoutWidth.toFloat())
+    val scaledHeight = min(size.height * ratio, layoutHeight.toFloat())
+    val scaledOffsetX = ((layoutWidth - scaledWidth) / 2f)
+    val scaledOffsetY = ((layoutHeight - scaledHeight) / 2f)
 
-fun ImageObjectInfo.calculateLayout(width: Float, height: Float): List<ImageObjectLayoutInfo> {
-    if (abs(size.width - width) < 1e-3 && abs(size.height - height) < 1e-3) {
-        return objects.map {
-            ImageObjectLayoutInfo(
-                imageObject = it,
-                boxOffset = it.offset,
-                boxSize = it.size
-            )
-        }
-    } else {
-        val ratio = min(width / size.width, height / size.height)
-        val scaledWidth = min(size.width * ratio, width)
-        val scaledHeight = min(size.height * ratio, height)
-        val scaledOffset = Offset((width - scaledWidth) / 2f, (height - scaledHeight) / 2f)
-        return objects.map {
-            ImageObjectLayoutInfo(
-                imageObject = it,
-                boxOffset = it.offset * ratio + scaledOffset,
-                boxSize = it.size * ratio
-            )
-        }
+    return objects.map {
+        val x = (it.offset.x * ratio + scaledOffsetX).roundToInt()
+        val y = (it.offset.y * ratio + scaledOffsetY).roundToInt()
+        val w = (it.size.width * ratio).roundToInt()
+        val h = (it.size.height * ratio).roundToInt()
+        ImageObjectLayoutInfo(
+            imageObject = it,
+            boxOffset = IntOffset(x, y),
+            boxSize = IntSize(w, h)
+        )
     }
 }
